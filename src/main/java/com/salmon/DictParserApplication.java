7bd6540d47806af6a14cbcc89e9e024845ccab89
Salmon <1179732961@qq.com>
2023-08-27 22:50:48 +0800

    初始化项目

---
+++ src/main/java/com/salmon/DictParserApplication.java
@@ -0,0 +1,13 @@
+package com.salmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salmon/DictParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
